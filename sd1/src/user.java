64a0aa0e3966bd40c02b493b3562893e7edeec96
plucas86 <plucas86@gmail.com>
2012-10-15 17:34:47 +0100

    comm
    comm

---
+++ sd1/src/user.java
@@ -0,0 +1,54 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author plucas
+ */
+
+import java.io.*;
+
+class user implements lists, Serializable {
+
+    private String id;
+    private StringBuffer notifications;
+    private transient DataInputStream in;
+    private transient DataOutputStream out;
+    int active;
+
+    public user(String _id, DataInputStream _in, DataOutputStream _out) {
+        id = _id;
+        active = 1;
+        notifications = new StringBuffer();
+        in = _in;
+        out = _out;
+        users.add(this);
+    }
+
+    public String getID() {
+        return id;
+    }
+
+    public StringBuffer getNot() {
+        return notifications;
+    }
+
+    public void setStream(DataInputStream _in, DataOutputStream _out) {
+        in = _in;
+        out = _out;
+    }
+
+    public void setNot(StringBuffer msg) {
+        notifications = msg;
+    }
+
+    public DataInputStream getINStream() {
+        return in;
+    }
+
+    public DataOutputStream getOUTStream() {
+        return out;
+    }
+}

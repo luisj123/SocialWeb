1c71b675c2940dc0eb385f4e510c5bfabb6c4bbe
plucas <plucas@REMNANT>
2012-10-16 15:43:28 +0100

    lol

---
+++ sd1/src/Data_lists.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author plucas
+ */
+import java.util.*;
+
+interface Data_lists{
+    
+    ArrayList<User> users = new ArrayList<User>();
+    ArrayList<Post> posts = new ArrayList<Post>();
+    
+}

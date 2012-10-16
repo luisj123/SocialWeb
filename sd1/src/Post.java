436c12200bb33d431d4a4c9fb5c210a4aac9ec1d
luis <lafj@student.dei.uc.pt>
2012-10-16 17:31:29 +0100

    date + time

--- sd1/src/Post.java
+++ sd1/src/Post.java
@@ -1,10 +1,5 @@
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
+import java.sql.*;
 import java.util.Calendar;
 
 public class Post {
@@ -18,7 +13,7 @@ public class Post {
     private Connection connection;
     private Statement statement;
     private ResultSet resultSet;
-    
+    private PreparedStatement st;
     
     Post(String _text){
         text = _text;
@@ -40,9 +35,19 @@ public class Post {
             Class.forName("org.sqlite.JDBC");  
             connection = DriverManager.getConnection("jdbc:sqlite:sd.db");  
             statement = connection.createStatement();  
-            DateFormat formato = new SimpleDateFormat("yyyy-MM-dd");  
-            formato.format(date);
-            resultSet = statement.executeQuery("insert into post values ('','"+ Author.getID() +"','"+ text +"',"+formato+");");
+            String INSERT_RECORD = "insert into posts (author, object, date, time) values(?,?,?,?)";
+    
+            st = connection.prepareStatement(INSERT_RECORD);
+            st.setString(1, Author.getID());
+            st.setString(2, text);
+            java.sql.Date sqlDate = new java.sql.Date(date.getTime().getTime());
+            st.setDate(3, sqlDate);
+            java.sql.Time sqlTime = new java.sql.Time(date.getTime().getTime());
+            st.setTime(4, sqlTime);
+            
+            st.executeUpdate();
+            
+            connection.close();
 
         } catch (Exception e){
             System.out.println("Erro: "+e);
